d7f53b6b4c3f302489f32719ae16cff95ba3212e
aumitcelik <aumitcelik@gmail.com>
2022-12-19 19:15:01 +0300

    first commit

---
+++ variables/Program.cs
@@ -0,0 +1,51 @@
+﻿
+byte testByte = 254;//1 bayt (0-255)
+System.Console.WriteLine(testByte);
+
+sbyte testSbyte = -127; // 1 bayt (-128-127)
+System.Console.WriteLine(testSbyte);
+
+short testShort = 18;// 2 bayt 
+System.Console.WriteLine(testShort);
+
+int testInt = 25;// 4 bayt
+System.Console.WriteLine(testInt);
+
+long testLong = 14;// 8 bayt
+System.Console.WriteLine(testLong);
+
+float testFloat = 4.7F;
+System.Console.WriteLine(testFloat);
+
+double testDouble = 7D;
+System.Console.WriteLine(testDouble);
+
+decimal testDecimal = 2454;
+System.Console.WriteLine(testDecimal);
+
+char testChar = 'A';
+System.Console.WriteLine(testChar);
+
+string testString = "Lorem ıpsum";
+System.Console.WriteLine(testString);
+
+string testString2 = string.Empty;
+System.Console.WriteLine(testString2);
+
+bool testBool = 06 > 01;
+System.Console.WriteLine(testBool);
+
+bool testBool2 = 1000 < 357;
+System.Console.WriteLine(testBool2);
+
+object testObject = "celik";
+System.Console.WriteLine(testObject);
+
+object testObject2 = 4534;
+System.Console.WriteLine(testObject2);
+
+DateTime testDate = DateTime.Now;
+System.Console.WriteLine(testDate);
+
+const float pi = 3.14F;
+System.Console.WriteLine(pi);
