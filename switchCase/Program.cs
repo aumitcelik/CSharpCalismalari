d6c0764ddcc78407d5c1592a3100ea717bc228cf
aumitcelik <aumitcelik@gmail.com>
2023-01-03 20:46:17 +0300

    switchCase

---
+++ switchCase/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace switchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
