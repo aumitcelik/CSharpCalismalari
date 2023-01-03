ff6bf39f09ab769ec57d34fa079ce0474dbcd170
aumitcelik <aumitcelik@gmail.com>
2023-01-03 20:21:07 +0300

    if_else

---
+++ if/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+        string sonuc = time>=6 && time<11 ? "Gunaydin": time<= 18 ? "İyi Günler": "İyi Geceler";
+        Console.WriteLine(sonuc);
+    }
+}
+}
