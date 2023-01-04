2af76659028621ecd43ee0ac30ec12717bf59e2d
aumitcelik <aumitcelik@gmail.com>
2023-01-04 19:43:03 +0300

    addWhileForEach

--- loops/Program.cs
+++ loops/Program.cs
@@ -6,14 +6,31 @@ namespace loops
     {
         static void Main(string[] args)
         {
+            //for
             for (int i = 1; i <= 10; i++)
             {
                 if (i % 2 == 0)
                     continue;
                 Console.WriteLine(i);
-                if (i==7)
-                break;
+                if (i == 7)
+                    break;
             }
+            //while
+            int sayac = 1;
+            while (sayac <= 5)
+            {
+                Console.WriteLine(sayac);
+                sayac++;
+            }
+            //ForEach
+            int[] sayiDizisi = { 2, 4, 5, 1, 2, 4 };
+            int toplam = 0;
+
+            foreach (int i in sayiDizisi)
+            {
+                toplam += i;
+            }
+            Console.WriteLine(toplam);
         }
     }
 }
