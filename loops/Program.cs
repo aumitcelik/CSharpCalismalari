e4a7d50a92d2ea87cd68395f765ab0aa670ea63a
aumitcelik <aumitcelik@gmail.com>
2023-01-04 19:14:15 +0300

    add loops

---
+++ loops/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                    continue;
+                Console.WriteLine(i);
+                if (i==7)
+                break;
+            }
+        }
+    }
+}
