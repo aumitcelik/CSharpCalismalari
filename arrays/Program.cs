6acec9ecc0290eaf5e35418e4fc6ea3cdec1136e
aumitcelik <aumitcelik@gmail.com>
2023-01-06 19:18:18 +0300

    updateArray

--- arrays/Program.cs
+++ arrays/Program.cs
@@ -6,19 +6,73 @@ namespace arrays
     {
         static void Main(string[] args)
         {
-            Console.Write("Lütfen dizinin eleman sayısını girin");
-            int elemanSayisi = Convert.ToInt32(Console.ReadLine());
-            int[] sayiDizisi = new int[elemanSayisi];
-            int toplam = 0;
-            for (int i = 0; i < sayiDizisi.Length; i++)
+            /* 1. Ders 
+                        Console.Write("Lütfen dizinin eleman sayısını girin");
+                        int elemanSayisi = Convert.ToInt32(Console.ReadLine());
+                        int[] sayiDizisi = new int[elemanSayisi];
+                        int toplam = 0;
+                        for (int i = 0; i < sayiDizisi.Length; i++)
+                        {
+                            Console.Write("Lütfen {0}. sayıyı girin:", i + 1);
+                            sayiDizisi[i] = Convert.ToInt32(Console.ReadLine());
+                            toplam += sayiDizisi[i];
+                        }
+                        double ortalama = toplam / elemanSayisi;
+                        Console.WriteLine("Sayıların ortalaması: " + ortalama);
+                        Console.ReadLine();
+                        */
+            int[] sayiDizisi = { 23, 12, 4, 86, 72, 3, 11, 17 };
+
+            Console.WriteLine("-------Sırasız Dizi------");
+            foreach (var item in sayiDizisi)
             {
-                Console.Write("Lütfen {0}. sayıyı girin:", i + 1);
-                sayiDizisi[i] = Convert.ToInt32(Console.ReadLine());
-                toplam += sayiDizisi[i];
+                Console.WriteLine(item);
             }
-            double ortalama = toplam / elemanSayisi;
-            Console.WriteLine("Sayıların ortalaması: " + ortalama);
-            Console.ReadLine();
+
+            Console.WriteLine("-------Sıralı Dizi------");
+            Array.Sort(sayiDizisi);
+
+            foreach (var item in sayiDizisi)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //Clear
+            Console.WriteLine("-------Array Clear------");
+
+            Array.Clear(sayiDizisi, 4, 3);
+            foreach (var item in sayiDizisi)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Reverse
+            Console.WriteLine("-------Array Reverse------");
+            Array.Reverse(sayiDizisi);
+            foreach (var item in sayiDizisi)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //İndexOf
+            Console.WriteLine("-------Array IndexOf------");
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 12));
+
+
+            //Resize
+            Console.WriteLine("-------Array Resize------");
+            Array.Resize<int>(ref sayiDizisi, 9);
+            sayiDizisi[8] = 06;
+
+            foreach (var item in sayiDizisi)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
         }
     }
 }
