982551ac7a33614fb95c2b47a65b07ad16826f41
aumitcelik <aumitcelik@gmail.com>
2023-01-06 16:27:04 +0300

    addArrays

---
+++ arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen dizinin eleman sayısını girin");
+            int elemanSayisi = Convert.ToInt32(Console.ReadLine());
+            int[] sayiDizisi = new int[elemanSayisi];
+            int toplam = 0;
+            for (int i = 0; i < sayiDizisi.Length; i++)
+            {
+                Console.Write("Lütfen {0}. sayıyı girin:", i + 1);
+                sayiDizisi[i] = Convert.ToInt32(Console.ReadLine());
+                toplam += sayiDizisi[i];
+            }
+            double ortalama = toplam / elemanSayisi;
+            Console.WriteLine("Sayıların ortalaması: " + ortalama);
+            Console.ReadLine();
+        }
+    }
+}
