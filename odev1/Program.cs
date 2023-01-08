e3bf88e902c5209856edc1624da326156b3d54d1
aumitcelik <aumitcelik@gmail.com>
2023-01-09 00:18:49 +0300

    add odev1

---
+++ odev1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodlar odev = new Metodlar();
+            
+            //Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+            odev.soru1();
+        }
+
+
+    }
+    class Metodlar
+    {
+        public void soru1()
+        {
+            Console.WriteLine("pozitif bir sayı girin ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 0)
+            {
+                int[] x = new int[n];
+                for (int i = 0; i < x.Length; i++)
+                {
+                    Console.Write("Lütfen {0}. pozitif sayıyı girin:", i + 1);
+                    x[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine("Çift Sayılar");
+                for (int i = 0; i < x.Length; i++)
+                {
+                    if (x[i] % 2 == 0 && x[i] > 0)
+                    {
+                        Console.WriteLine(x[i]);
+                    }
+                }
+            }
+            else
+                soru1();
+        }
+    }
+}
