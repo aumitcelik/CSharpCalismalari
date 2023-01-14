8885fac4e90320a3a07d5146eea2045719ea7674
aumitcelik <aumitcelik@gmail.com>
2023-01-15 00:10:45 +0300

    addsoru3

---
+++ odev2/koleksiyonlarSoru3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+namespace koleksiyonlarSoru2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cümle girin:");
+            string input = Console.ReadLine().ToLower(); // cümleyi küçük harfe çeviriyoruz
+            string vowels = "euıüaiöo"; // sesli harfler
+            char[] vowelsInInput = input.Where(c => vowels.Contains(c)).ToArray(); // sesli harfleri diziye atıyoruz
+            Array.Sort(vowelsInInput); // dizi içindeki elemanları alfabetik olarak sıralıyoruz
+            Console.WriteLine("Sesli harfler: " + String.Join(", ", vowelsInInput)); // sesli harfleri yazdırıyoruz
+        }
+    }
+}
